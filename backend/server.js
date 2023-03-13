031843806b93a154a9f231e7b1be7a9a5f92cb1b
Karensaf <karensota1913@gmail.com>
2023-03-12 22:32:40 -0600

    Doc: Añado un ejemplo de comentario

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
-
-
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
-
+//?El sensei Daniel es la onda :)
 connectDB()
 
 const app = express()
